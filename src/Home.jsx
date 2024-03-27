185aee2e000e784861ff8c3c4bdcbcebc7e755fc
moustafa-abas <124290544+moustafa-abas@users.noreply.github.com>
2024-03-27 16:20:44 +0200

    finish

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,27 +11,29 @@ return (
 
     
 <Header/>
-<landing className="landing d-flex flex-sm-row flex-column flex-wrap  justify-content-between  ">
+<landing className="landing d-flex flex-sm-row flex-column flex-wrap flex-md-nowrap   ">
     <aside className="d-flex d-sm-block justify-content-between">
         <div className="info ">
 <h1>hello,<span>i&apos; am</span> </h1>
 <h1> mostafa abas</h1> 
 <h1 className="mb-5"><span> front end developer</span></h1>
 </div>
-<div className="  d-flex align-items-center ">
+<div className="dl mb-3  d-flex align-items-center ">
 <FontAwesomeIcon icon={faDownload}   className="icon download py-2 fs-3 px-2"/>
-<a href="./assets/mostafa abas cv.pdf" download='mostafa abas cv.pdf' className=" text-decoration-none fs-5 py-2  px-3 text-capitalize"> download cv</a>
+<a href="./assets/mostafa abas cv.pdf" download='mostafa abas cv.pdf' className="dl text-decoration-none fs-5 py-2  px-3 text-capitalize"> download cv</a>
 </div>
     </aside>
-    <main className=" px-5 position-relative">
-<img src={me} alt=""  className="me  w-100 p-3 rounded-circle "/>
+    <main className=" position-relative me-">
+        <div className="circle">
+<img src={me} alt=""  className="me  w-100  rounded-circle "/>
 <a target='_blank' href =  "https://www.linkedin.com/in/moustafa-3bas-36a316267?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bmq5Ca5WKTtuXSHPGmSZw6g%3D%3D" className="link1" > <img src={linkedIn} alt="" className="icon1 " /></a>
 <a target='_blank' href =  "https://www.facebook.com/mostafa.abas8?mibextid=ZbWKwL" className="link2"> <img src={facebook} alt="" className="icon2 "/></a>
 <a target='_blank'  href =  "https://wa.me/+201280163605? " className="link3"> <img src={whatsApp} alt=""  className="icon3 "/></a>
+</div>
     </main>
-    <aside className=" ps-5 d-none  d-md-block">
-        <h2 className="fs-4 p-2  ">4 projects</h2>
-        <h2 className="fs-4 p-2  ex">1 year experience</h2>
+    <aside className="  d-none  d-md-block">
+        <h2 className="fs-4 p-2 ms-4 ">4 projects</h2>
+        <h2 className="fs-4 p-2 ms-4 ex">1 year experience</h2>
     </aside>
 </landing>
 </home>
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,21 +1,26 @@
 import Header from "./Header"
-import carRental from'./assets/car rental.png'
-import restaurant from'./assets/resturant.png'
-import elipso from'./assets/elipso.png'
+import carRental from'./assets/car rental.webp'
+import restaurant from'./assets/resturant.webp'
+import elipso from'./assets/elipso.webp'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 const Projects = () => {
 
   return (
     <>
     <Header/>
-    <div className="projects d-flex flex-column flex-sm-row flex-wrap mt-2  ">
-      <div className="card position-relative w-100 - border-0 my-4">
-        <a href="https://elipso-project.vercel.app/" target="_blank" ><img src={elipso} alt="" className="w-100" /></a>
+    <div className="projects d-flex flex-column flex-sm-row flex-wrap   mt-2  ">
+      <div className="card  w-100   border- my-4">
+        <img src={elipso} alt="" className="w-100" />
+        <a href="https://elipso-project.vercel.app/" target="_blank" >  <FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
       </div>
-      <div className="card position-relative w-100 border-0 my-4">
-        <a href="https://restaurant-kohl-tau.vercel.app/" target="_blank" ><img src={restaurant} alt="" className="w-100" /></a>
+      <div className="card  w-100 border-0 my-4">
+      <img src={restaurant} alt="" className="w-100" />
+        <a href="https://restaurant-kohl-tau.vercel.app/" target="_blank" > <FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
         </div>
-        <div className="card position-relative w-100 border-0 my-4">
-        <a href="https://car-rental-ashy.vercel.app/" target="_blank" ><img src={carRental} alt="" className="w-100" /></a>
+        <div className="card  w-100 border-0 my-4">
+        <img src={carRental} alt="" className="w-100" />
+        <a href="https://car-rental-ashy.vercel.app/" target="_blank" ><FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
         </div>        
     </div>
     </>
