de187abbf4e184ca66aa8fc84769f2ac2cf4a83a
moustafa-abas <124290544+moustafa-abas@users.noreply.github.com>
2024-06-17 03:43:39 +0300

    add fourth project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,6 +2,7 @@ import Header from "./Header"
 import carRental from'./assets/car rental.webp'
 import restaurant from'./assets/resturant.webp'
 import elipso from'./assets/elipso.webp'
+import e_commerce from'./assets/e-commerce.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 const Projects = () => {
@@ -10,6 +11,10 @@ const Projects = () => {
     <>
     <Header/>
     <div className="projects d-flex flex-column flex-sm-row flex-wrap   mt-2  ">
+    <div className="card  w-100 border-0 my-4">
+        <img src={e_commerce} alt="" className="w-100" />
+        <a href="https://e-commerce-ewjztzokw-moustafa-abas-projects.vercel.app/" target="_blank" ><FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
+        </div>     
       <div className="card  w-100   border- my-4">
         <img src={elipso} alt="" className="w-100" />
         <a href="https://elipso-project.vercel.app/" target="_blank" >  <FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
@@ -22,6 +27,7 @@ const Projects = () => {
         <img src={carRental} alt="" className="w-100" />
         <a href="https://car-rental-ashy.vercel.app/" target="_blank" ><FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
         </div>        
+   
     </div>
     </>
   )
