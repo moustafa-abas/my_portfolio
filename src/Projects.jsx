d2e472e61f5590098057af1d9dcdfb6799f0defa
moustafa-abas <124290544+moustafa-abas@users.noreply.github.com>
2024-06-24 23:29:14 +0300

    update project link

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
     <div className="projects d-flex flex-column flex-sm-row flex-wrap   mt-2  ">
     <div className="card  w-100 border-0 my-4">
         <img src={e_commerce} alt="" className="w-100" />
-        <a href="https://e-commerce-ewjztzokw-moustafa-abas-projects.vercel.app/" target="_blank" ><FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
+        <a href="https://e-commerce-lilac-five.vercel.app/" target="_blank" ><FontAwesomeIcon icon={faEye} fade style={{color:"#fff"}} className="icon"/></a>
         </div>     
       <div className="card  w-100   border- my-4">
         <img src={elipso} alt="" className="w-100" />
